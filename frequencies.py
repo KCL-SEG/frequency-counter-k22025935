d1a90dca380674cfd1fcf58271e96d7772281f85
Caleb Chan <caleb.chan@kcl.ac.uk>
2023-10-10 16:19:59 +0100

    Create Frequency Method

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,12 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
+    frequencies = dict()
+    for item in items:
+        itemAsString = str(item)
+        #check if item in dict
+        if itemAsString in frequencies:
+            frequencies[itemAsString] +=1
+        else:
+            frequencies[itemAsString] = 1
     return frequencies
